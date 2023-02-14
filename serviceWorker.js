b23b0bf13e781773d7e578312b897ddaa7546483
Brian Twene <twene521@gmail.com>
2023-02-14 21:47:05 +0000

    some moar changes 2

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,15 +1,19 @@
 const colorGenAssets = "colorGenAssets"
 
-window.addEventListener('beforeinstallprompt', e => {
-    console.log('beforeinstallprompt Event fired');
-    e.preventDefault();
-    // Stash the event so it can be triggered later.
-    this.deferredPrompt = e;
-    return false;
-});
-// When you want to trigger prompt:
-this.deferredPrompt.prompt();
-this.deferredPrompt.userChoice.then(choice => {
-    console.log(choice);
-});
-this.deferredPrompt = null;
+
+
+if (typeof window !== 'undefined') {
+    window.addEventListener('beforeinstallprompt', e => {
+        console.log('beforeinstallprompt Event fired');
+        e.preventDefault();
+        // Stash the event so it can be triggered later.
+        this.deferredPrompt = e;
+        return false;
+    });
+    // When you want to trigger prompt:
+    this.deferredPrompt.prompt();
+    this.deferredPrompt.userChoice.then(choice => {
+        console.log(choice);
+    });
+    this.deferredPrompt = null;
+}
